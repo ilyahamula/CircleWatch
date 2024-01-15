48c15be3385c7996b3cacb62f6ca1e9f0900bec3
ilyahamula <ilyahamula95@gmail.com>
2024-01-15 18:49:48 +0200

    Store brightness in eeprom

--- include/FlashMemory.h
+++ include/FlashMemory.h
@@ -7,8 +7,9 @@ enum class eAddres
     HoursColorAddr = 0, // +2 for g,b
     MinutesColorAddr = 3, // +2 for g,b
     ModeAddr = 6,
-
-    Size = 7,
+    DialBrightness = 7,
+    
+    Size = 8,
 };
 
 class FlashMemory
